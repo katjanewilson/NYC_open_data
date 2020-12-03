c04f3fffe7fad5439d67668f97d7bdd15e234eef
Kat Wilson <katjanewilson@gmail.com>
2020-12-03 20:44:04 +0000

    edits

--- project_scripts/step_3_2_matching.R
+++ project_scripts/step_3_2_matching.R
@@ -9,7 +9,8 @@ working_data_graph <- working_data %>%
   filter(Demographic.Variable %in% c("SWD", "All Students",
                                      "White", "Black",
                                      "Male", "Female",
-                                     "Hispanic", "Poverty")) %>%
+                                     "Hispanic", "Poverty",
+                                     "Not SWD")) %>%
   group_by(self_contained_option, Demographic.Variable) %>%
   summarise(mean = mean(Percent_Attendance),
             mean_absent = mean(Percent_Chronically_Absent))
@@ -18,7 +19,7 @@ working_data_graph <- working_data_graph %>%
                                         "No Self-Contained Option"))
 ggplot(data = working_data_graph, aes(x = reorder(Demographic.Variable, -mean_absent),
                                 y = mean_absent,
-                                label = mean_absent)) +
+                                fill = "red")) +
          geom_bar(sta = "identity") +
   facet_wrap(~self_contained_option) +
   xlab ("Demographics") +
